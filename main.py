d79ecd0f4f05fd9f6806bead4b1357bb40452f8a
mikegertzzz <forbridden@gmail.com>
2023-12-28 16:13:51 +0300

    Added requirements.txt

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return "Привет! Это главная страница моего сайта."
+
 
 if __name__ == "__main__":
     app.run(debug=True)
